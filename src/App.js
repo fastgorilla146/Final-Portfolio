644082aed0ecf7da47955e9fd3293f53be359a52
sidarth-raman <sidarth_raman@brown.edu>
2022-12-13 14:54:08 -0500

    Final changes
    Ready for submission

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 
 
@@ -6,19 +5,18 @@ function App() {
   return (
       <div>
       <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://kit.fontawesome.com/31149d48b0.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <script src="hnttps://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://kit.fontawesome.com/31149d48b0.js" crossorigin="anonymous"></script>
 
   <nav class="navbar navbar-expand-lg fixed-top navbarScroll">
         <div class="container">
-            <a class="navbar-brand" href="#">Bruno's Portfolio</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <a class="nav-link" href="#">Bruno's Portfolio</a>
+            <button class="navbar-toggler" type="button">
             </button>
             
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div>
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item active">
                         <a class="nav-link" href="#home">Home</a>
@@ -40,17 +38,15 @@ function App() {
 
     <section class="bgimage" id="home">
         <div class="container-fluid">
-            <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
                 <h2 class="hero_title">Hi, it's me Bruno</h2>
                 <p class="hero_desc">I am a UI/UX Student at Brown University</p>
-            </div>
             </div>
         </div>
     </section>
 
     <section id="about">
-        <div class="container mt-4 pt-4">
+        <div class="containerSec mt-4 pt-4">
             <h1 class="text-center">About Me</h1>
             <div class="row mt-4">
                 <div class="col-lg-4">
@@ -74,7 +70,7 @@ function App() {
     </section>
 
     <section id="portfolio">
-        <div class="container mt-3">
+        <div class="containerSec mt-3">
             <h1 class="text-center">Portfolio</h1>
             <div class="row">
             <div class="col-lg-4 mt-4">
@@ -82,11 +78,11 @@ function App() {
                           <img class="card-img-top" src="https://www.bakingbusiness.com/ext/resources/2019/8/08192019/GlobalTrends.jpg?t=1566494557&width=1080"/>
                           <div class="card-body">
                               <h4 class="card-title">React Studio</h4>
-                              <p class="card-text">A redesigned website for Wayland Bakery.</p>
-                              <b>Context/Problem<br/></b>
-                                <b>Research/Feedback<br/></b>
-                                <b>Design Iterations<br/></b>
-                                <b>Conclusion<br/></b>
+                              <p class="card-text">A brand new website for Wayland Bakery.</p>
+                              <b>Context/Problem:</b> Created an interactive website where users can add items to their cart and the total shows up on the site. It could serve as a website for a bakery that is attempting to sell their product on their website and this portion would serve as the menu, add to cart, and checkout page. <br/>
+                                <b>Research/Feedback:</b> This website was good but not the best as there was no organization and lots of the feedback I got was that there should be more items shown per row to make it less spaced out. Also, the cart should be show at the top so users don't have to scroll down to the bottom to see the total every single time. <br/>
+                                <b>Design Iterations</b> There weren't many design iterations on this website but one of the main ones was being able to have an updating cart that shows how much of each product is in the cart. Previously, I received feedback that it is difficult to know what is in the cart without there being a total number at the bottom, so using state variables in React I was able to add this. <br/>
+                                <b>Conclusion:</b> While this website served its purpose there are some additional changes I could make to improved the aesthetic and usabiliity of this website. It should be easier to fit on one page and more organized with certain sorting and/or filtering tools. It should also have multiple pages and more information that users might come to the website to look for. <br/>
                               <div class="text-center">
                                   <a href="https://fastgorilla146.github.io/react-studio/" class="btn btn-success">Link</a>
                               </div>
@@ -100,10 +96,10 @@ function App() {
                           <div class="card-body">
                               <h4 class="card-title">Responsive Redesign</h4>
                               <p class="card-text">A redesign for Berkshire Hathaway's corporate website.  </p>
-                                <b>Context/Problem<br/></b>
-                                <b>Research/Feedback<br/></b>
-                                <b>Design Iterations<br/></b>
-                                <b>Conclusion<br/></b>
+                                <b>Context/Problem:</b> I had to choose a website which was not up to standards with modern day sites and create a lo-fi, hi-fi, and finally a landing page for the website. I had to be able to show all the features of the Berkshire Hathaway website in an improved manner. <br/>
+                                <b>Research/Feedback:</b> I realized that even in my website it wasn't as organized as it could fully be because the data itself wasn't organized. Sometimes for the improvement of a website that holds lots of data, the data itself has to be stored in an organized manner that cna be shown in the interface and users can sort/filter on. This wasn't the case with Berkshire so even though I redesigned it, I had to make assumptions that this could be done with lots of the hyperlinks on their page.  <br/>
+                                <b>Design Iterations:</b> In my first couple iterations I wasn't able to cleanly show that there are multiple sections where other users can click. Then I added a section at the top showing that users can click to choose what content was displayed on their page and this made it more effective. <br/>
+                                <b>Conclusion</b> In concolusion, I realized redesigning a page is often easy, but there are many little things that can make it difficult. Not all websites have enough content to be split into many different subpages, but it isn't aesthetically pleasing to have everything on one page either. <br/>
                             
                               <div class="text-center">
                                   <a href="https://fastgorilla146.github.io/responsive-redesign-assignment/" class="btn btn-success">Link</a>
@@ -119,10 +115,10 @@ function App() {
                           <div class="card-body">
                               <h4 class="card-title">Development</h4>
                               <p class="card-text">Interactive shopping center for Brown University.</p>
-                              <b>Context/Problem<br/></b>
-                                <b>Research/Feedback<br/></b>
-                                <b>Design Iterations<br/></b>
-                                <b>Conclusion<br/></b>
+                              <b>Context/Problem: </b>I was creating a fully functional React website that allows users to add items to cart, create a wishlist, and sort and filter products accordingly. It had many simple features that would be available on a regular shopping stores.  <br/>
+                                <b>Research/Feedback:</b> I received lots of feedback into how the structure of an e-commerce site should look. While usability, and functionality is the highest priority, there should also be some tactics to try and get users to buy more. In the future showing special edition or on sale items would be higher up.<br/>
+                                <b>Design Iterations:</b> My main design iterations included creating a better structure for the wish list and adding more sorting features. <br/>
+                                <b>Conclusion:</b> My main takeaways from this was the importance of creating a good structure for the data that is being displayed since I realized iterating on that and creating sorting and filtering options is very important. <br/>
                               <div class="text-center">
                                   <a href="https://fastgorilla146.github.io/development/" class="btn btn-success">Link</a>
                               </div>
@@ -140,6 +136,10 @@ function App() {
                         <div class="card-body">
                             <h4 class="card-title">Personas</h4>
                             <p class="card-text">Interactive study observing the interface of printers at Brown University libraries.</p>
+                            <b>Context/Problem: </b> I was doing a study on the printers at Brown University's Science Library. I was able to obverse how users interact and work with the printers to see if it is a good interface.  <br/>
+                                <b>Research/Feedback:</b> During my time reasearching this I ws able to see hwo different personas interacted with the interface and this helped me see how different users might interact differently with certain types of UIs and how I should cater mine to all types of people.<br/>
+                                <b>Design Iterations:</b> There weren't too many design iterations on this but the design was inspired to tell a story. <br/>
+                                <b>Conclusion:</b> In conclusion I was able to see how many different types of personas can affect their interactions with different interfaces and how it should be catered towards everyone.  <br/>
                             <div class="text-center">
                                 <a href="https://fastgorilla146.github.io/personas/" class="btn btn-success">Link</a>
                             </div>
@@ -164,13 +164,6 @@ function App() {
 
                 <label for="lname">Last Name</label>
                 <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
-
-                <label for="country">Country</label>
-                <select id="country" name="country">
-                <option value="australia">Australia</option>
-                <option value="canada">Canada</option>
-                <option value="usa">USA</option>
-                </select>
 
                 <label for="subject">Subject</label>
                 <textarea id="subject" name="subject"></textarea>
